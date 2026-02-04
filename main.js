65fe1400d35fdc34a37415cea972b5b0ba5f6125
RjayBernales <rainieljevson0@gmail.com>
2026-02-04 16:56:07 +0800

    maek border for closer campus focused view

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ const CAMPUS_BOUNDS = L.latLngBounds(
   [8.365000, 124.876000]  
 );
 
-//Leaflet ni for map initialization
-const map = L.map("map").setView([8.49, 124.82], 11);
-
+//Leaflet ni for map initialization v2 (naanay panning )
+const map = L.map("map", {
+  center: CAMPUS_CENTER,
+  zoom: 17,
+  minZoom: 16,
+  maxZoom: 19,
+  maxBounds: CAMPUS_BOUNDS,
+  maxBoundsViscosity: 1.0 
+});
 L.tileLayer(
   "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
   {
@@ -17,3 +23,10 @@ L.tileLayer(
     maxZoom: 19
   }
 ).addTo(map);
+
+// Para ma mark ang border around campus since walay way para campus lang makita
+L.rectangle(CAMPUS_BOUNDS, {
+  color: "#0d6efd",
+  weight: 2,
+  fillOpacity: 0.05
+}).addTo(map).bindPopup("Northern Bukidnon State College");
