0dd044d2af52e09afeb3226d7425143cb0e5c86d
RjayBernales <rainieljevson0@gmail.com>
2026-02-04 16:49:05 +0800

    found a way to focus the map on the campus using long, lat

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// long,lat sa campus
+const CAMPUS_CENTER = [8.359999, 124.868103];
+
+// Define bounds sa campus paras campus ditso
+const CAMPUS_BOUNDS = L.latLngBounds(
+  [8.355000, 124.860000],
+  [8.365000, 124.876000]  
+);
+
+//Leaflet ni for map initialization
 const map = L.map("map").setView([8.49, 124.82], 11);
 
 L.tileLayer(
